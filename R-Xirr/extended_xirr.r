2ba015541ac287b5be32e02ad51913b6af460ae2
Sanjana <sanjana.mahesh.3294@gmail.com>
2016-03-02 17:44:35 +0530

    Made changes to existing xirr to handle transction types and optimize loops

--- R-Xirr/extended_xirr.r
+++ R-Xirr/extended_xirr.r
@@ -82,30 +82,52 @@ xirr <- function (cashflow, dates) {
 
 # Returns Data sorted on Name, units, and descending date
 sort_data <- function(data){
-  data <-  data[with(data,order(Name,-Units,Date)),]
+  data <-  data[with(data,order(SCHEMECODE,-Units,Date)),]
   data
 }
 
 # Finds unique fund names
 fund_names <- function(data){
-  a <- as.vector(unique(data[,1]))
+  a <- as.vector(unique(data[,'SCHEMECODE']))
 }
 
 # Returns a list of funds and their transactions
 fund_data <- function(data){
   a <- fund_names(data)
   y <- list()
+  amts <- c()
+  dates <- c()
+  count <- 1
+
   for(i in a){
     x <- rbind(data[1,])
     for(j in 1:nrow(data)){
-      if(data[j,'Name'] == i){
+      if(data[j,'SCHEMECODE'] == i){
         x <- rbind(x,data[j,])
       }
     }
     x <- x[-1,]
-    y[[i]] <- x
+    z <- remove_na(x)
+    print(paste("INDEX HERE - ",i," COUNT - ",count))
+    if(!is.null(z)){
+      z <- add_redemptions(z)
+      w <- knock_off(z)
+
+      if(!is.null(w)){
+        print(paste("SAVING HERE!"))
+        y[[count]] <- w
+        amts <- c(amts,as.vector(w[,"Amount"]))
+        dates <- c(dates,as.vector(w[,"Date"]))
+        count <- count + 1
+      }
+      # else{
+      #   list_funds[[i]] <- NULL
+      # }
+
+      # y[[i]] <- z
+    }
   }
-  y
+  return(list(y,amts,dates))
 }
 
 # Adds all redemptions of a set of transactions
@@ -121,44 +143,114 @@ add_redemptions <- function(a){
   a
 }
 
+
+# Removes zero value funds, and Dividend Payout transctions
+remove_na <- function(a){
+#    print(paste("I: ",i," length(a) ",length(a)))
+  j <- 1
+  while(j <= nrow(a)){
+    if(is.na(a[j,'Units'])){
+      a <- a[-j,]
+    }
+    else if((a[j,'TRANSTYPE']=="RED") && (a[j,'Units'] > 0)){
+      a[j,'Units'] <- (-1) * a[j,'Units']
+      j <- j+1
+    }
+    else if(a[j,'TRANSTYPE']=="DP"){
+      a <- a[-j,]
+    }
+    # else if(a[j,'TRANSTYPE']=="DR"){
+    #   a <- a[-j,]
+    # }
+    else{
+      j <- j+1
+    }
+  }
+  if(nrow(a) == 0){
+    return(NULL)
+  }
+  else if(round(sum(a[,'Units']),digits = 3) == 0 ){
+    return(NULL)
+  }
+  a
+}
+
+
 # Balances redemptions with purchases
 knock_off <- function(a){
   i <- 1
+  if(nrow(a)==1){
+    return(a)
+  }
   while((i < nrow(a)) && ((a[nrow(a),'Units'] + a[i,'Units']) < 0)){
-    print(paste("HERE1: ",a[nrow(a),'Units']," ",a[i,'Units']))
+#    print(paste("HERE1: ",a[nrow(a),'Units']," ",a[i,'Units']))
     a[nrow(a),"Units"] <- a[nrow(a),'Units'] + a[i,'Units']
     a <- a[-i,]
-    print(a)
+#    print(a)
     exp <- ((i < nrow(a)) && ((a[nrow(a),'Units'] + a[i,'Units']) < 0))
   }
 
   if(a[nrow(a),'Units']+a[i,'Units'] >= 0){
     print(paste("HELLO"))
+#    print(a)
     units <- a[i,'Units']
     amount <- as.numeric(gsub(",","",a[i,"Amount"]))
     a[i,'Units'] <- abs(a[nrow(a),'Units'] + a[i,'Units'])
     a[i,'Amount'] <- round((amount * (a[i,'Units']/units)),digits=2)
     a <- a[-nrow(a),]
-    if(a[1,'Units'] == 0){
-      a <- a[-1,]
+    if(a[i,'Units'] == 0){
+      a <- a[-i,]
+      if(nrow(a)==0){
+        print(paste("RETURNING NULL1"))
+        return(NULL)
+      }
     }
   }
+  
+  if(nrow(a)==0){
+    print(paste("RETURNING NULL2"))
+    return(NULL)
+  }
+  print(a)
   a
 }
 
 # Calculates extended XIRR
 ext_xirr <- function(data){
   data <- sort_data(data)
-  list_funds <- fund_data(data)
   amts <- c()
   dates <- c()
+  data[,"TRANSTYPE"] <- gsub("\\s*","",data[,'TRANSTYPE'])
+  data[,"TRANSTYPE"] <- gsub("SI","PURA",data[,'TRANSTYPE'])
+  data[,"TRANSTYPE"] <- gsub("SO","RED",data[,'TRANSTYPE'])
+  data[,"TRANSTYPE"] <- gsub("TI","PUR",data[,'TRANSTYPE'])
 
-  for(i in 1:length(list_funds)){
-    list_funds[[i]] <- add_redemptions(list_funds[[i]])
-    list_funds[[i]] <- knock_off(list_funds[[i]])
-    amts <- c(amts,as.vector(list_funds[[i]][,"Amount"]))
-    dates <- c(dates,as.vector(list_funds[[i]][,"Date"]))
-  }
+    results <- fund_data(data)
+
+  list_funds <- results[[1]]
+  amts <- results[[2]]
+  dates <- results[[3]]
+  # list_funds <- remove_na(list_funds)
+
+  i <- 1
+  y <- list()
+  count <- 1
+
+  # while(i <= length(list_funds)){
+  #   list_funds[[i]] <- add_redemptions(list_funds[[i]])
+  #   print(paste("INDEX HERE - ",i))
+  #   x <- knock_off(list_funds[[i]])
+  #   if(!is.null(x)){
+  #     y[[count]] <- x
+  #     count <- count + 1
+  #     amts <- c(amts,as.vector(x[,"Amount"]))
+  #     dates <- c(dates,as.vector(x[,"Date"]))
+  #     i <- i+1
+  #   }
+  #   else{
+  #     list_funds[[i]] <- NULL
+  #   }
+  # }
 
   amts <- as.numeric(gsub(",","",amts))
 
@@ -170,11 +262,12 @@ ext_xirr <- function(data){
   amts <- as.numeric(gsub(",","",table[,1]))
   dates <- table[,2]
 
+  print(paste("SUM - ",sum(amts)))
   number <- readline(prompt="Enter present value: ")
   number <- (as.numeric(number)) * (-1)
 
   amts <- c(amts,number)
-  dates <- c(dates,format(Sys.Date(),"%d-%m-%y"))
+  dates <- c(dates,format(Sys.Date(),"%d-%m-%Y"))
 
   xirr1 <- xirr(amts,dates)
   print(paste("XIRR: ",xirr1))
